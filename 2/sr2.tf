db33029e1564fc8d52510fda7102d984def77aaf
sivareddy407 <88810375+sivareddy407@users.noreply.github.com>
2022-01-18 00:37:57 +0530

    Update sr2.tf

--- 2/sr2.tf
+++ 2/sr2.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
 }
-#this is ec2 instance
+ # this is ec2 instance
   resource "aws_instance" "siva" {
   ami           = "ami-04505e74c0741db8d"
   instance_type = "t2.micro"
