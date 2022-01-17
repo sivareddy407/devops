7ad8584f9bbf371105ff9db81e30837f4dda5d8f
sivareddy407 <88810375+sivareddy407@users.noreply.github.com>
2022-01-18 00:28:01 +0530

    Update sr2.tf

--- 2/sr2.tf
+++ 2/sr2.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-  resource "aws_instance" "siva" {
+  resource "aws_instance" "1siva" {
   ami           = "ami-04505e74c0741db8d"
   instance_type = "t2.micro"
   key_name      = "SR"
